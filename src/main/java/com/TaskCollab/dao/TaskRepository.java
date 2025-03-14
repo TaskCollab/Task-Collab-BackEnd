b97ac76cd091fed23c35a2ad715d0c56b5003a09
d2r3v <drv792@gmail.com>
2025-03-13 17:09:24 -0700

    Impl Search and Filter for Tasks

--- src/main/java/com/TaskCollab/dao/TaskRepository.java
+++ src/main/java/com/TaskCollab/dao/TaskRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,4 +21,5 @@ List<Task> findByAssigned_To(@Param("assignedTo") String assignedTo);
 
 @Query("SELECT t FROM Task t WHERE t.task_Id = :task_Id")
 Optional<Task> findById(@Param("task_Id") Long task_Id);
+
 }
